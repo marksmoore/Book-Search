f92eec662f7f5a9c277c574cee82936fb5b70a5a
Marco <msmo1960@gmail.com>
2021-08-15 13:42:09 -0500

    many updates, too many

--- client1/src/components/SignupForm.js
+++ client1/src/components/SignupForm.js
@@ -3,8 +3,6 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
-import { useMutation } from '@apollo/react-hooks';
 
 const SignupForm = () => {
   // set initial form state
